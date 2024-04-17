7d29925ca9d00c8db880207326288c0a3d48ec1d
deocampoo <deniseeocampoo.00@gmail.com>
2024-04-17 13:32:36 -0300

    Uso de api TMDB

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import { LoginButton } from './components/Login';
 import { LogoutButton } from './components/Logout';
 import { Profile } from './components/Profile'; 
-import  Logo  from './components/Logo';
-
-import './App.css';
-
+import  Logo    from './components/Logo';
+import MovieApi from './components/movieApi';
 function App() {
   const {isAuthenticated} = useAuth0();
+
   return (
     <div className='App'>
-      <header className="App-header">
-
         {isAuthenticated ? (
           <>
-            <Profile/>
-            <LogoutButton />
+            {/*<Profile/>*  <LogoutButton />*/}
+            <MovieApi />
+
           </>
         ) : (
           <div>
           <Logo />
           <LoginButton  />
           </div>
-
         )}
-      </header>
+     
     </div>
+
+
+
   );
 }
 
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 const Logo = () => {
   return <img src="/MovieHUB.jpg" alt="MovieHUB Logo" className='moviehubLogo' />;
-
-
 }
 
 export default Logo
